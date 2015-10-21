1463d97a2ce8a21df0e2d8855be94ad6c32f7769
Joel <SuperSpaceNinja@users.noreply.github.com>
2015-10-21 10:15:15 +0200

    User specific list stuff

--- src/main/java/service/ListDAO.java
+++ src/main/java/service/ListDAO.java
@@ -12,16 +12,15 @@ import model.TopList;
 
 @Component
 public class ListDAO {
-	
+
 	private static final String PERSISTENCE_UNIT_NAME = "TopListan";
 	private EntityManagerFactory factory;
 	private EntityManager em;
-	
-	public ListDAO (){
-		
+
+	public ListDAO() {
+
 	}
-	
-	
+
 	public void openConnection() {
 		try {
 			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
@@ -35,28 +34,42 @@ public class ListDAO {
 	public void closeConnection() {
 		em.close();
 	}
-	
-	public List<TopList> getListItems(){
+
+	public List<TopList> getListItems() {
 		openConnection();
-		TypedQuery<TopList> createQuery = em.createQuery(
-				"SELECT c FROM TopList c", TopList.class);
-	
+		TypedQuery<TopList> createQuery = em.createQuery("SELECT c FROM TopList c", TopList.class);
+
 		List<TopList> resultList = createQuery.getResultList();
 		closeConnection();
-		
+
 		return resultList;
-		
+
 	}
-	
-	public void addListItemToDB(TopList item){
-		try{
-			System.out.println("addar"+ item.getProduct());
+
+	/***
+	 * Get logged in users topList from DB
+	 * 
+	 * @author Joel
+	 * @param userId the logged in users id
+	 * @return user specific topList
+	 */
+	public List<TopList> getUserListItems(int userId) {
 		openConnection();
-		em.getTransaction().begin();
-		em.persist(item);
-		em.getTransaction().commit();
-		closeConnection();
-		}catch(Exception e){
+		TypedQuery<TopList> createQuery = em.createQuery("SELECT t FROM TopList t WHERE t.userId = :userId", TopList.class)
+				.setParameter("userId", userId);
+		List<TopList> usersList = createQuery.getResultList();
+		return usersList;
+	}
+
+	public void addListItemToDB(TopList item) {
+		try {
+			System.out.println("addar" + item.getProduct());
+			openConnection();
+			em.getTransaction().begin();
+			em.persist(item);
+			em.getTransaction().commit();
+			closeConnection();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
